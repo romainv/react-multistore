3a986192cf1471f07a182dcc644696125078ee15
romain <romain.vermeulen@gmail.com>
2021-08-17 15:13:09 +0800

    Added an example usage of useApi and fixed Github action

--- example/index.js
+++ example/index.js
@@ -1,14 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { StoreProvider } from "./store"
+import { useApi } from "../src"
 import Likes from "./Likes"
 import Views from "./Views"
 
 function App() {
+  // Demonstrate the use of a local apiFactory
+  const { isVisible, toggle } = useApi(
+    (state, setState) => ({
+      ...state,
+      toggle() {
+        setState({ isVisible: !state.isVisible })
+      },
+    }),
+    { isVisible: true } // Initial state
+  )
   return (
     <React.Fragment>
       <Likes />
-      <Views />
+      <br />
+      <button onClick={() => toggle()}>Toggle view count</button>
+      {isVisible && <Views />}
     </React.Fragment>
   )
 }
